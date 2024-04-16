e7909417b02f19c51ac0a4eb2f2c2bded76515cb
Jchips <20979096+Jchips@users.noreply.github.com>
2024-04-15 19:33:28 -0700

    created basic server

---
+++ __tests__/server.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { app } = require('../src/server');
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe('Server test', () => {
+  test('Default route is working', async () => {
+    const response = await request.get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.text).toEqual('default route is working');
+  });
+});
